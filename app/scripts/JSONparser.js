feaf6f3f392cbb48bea9172a220e067110268762
David Rose <david@sxsw.com>
2012-10-05 10:07:27 -0500

    added js files and refactored a bit

---
+++ app/scripts/JSONparser.js
@@ -0,0 +1,20 @@
+
+
+var openXCStartLatitude = JSON.parse('{"name":"latitude","value":42.291508}');
+var openXCStartLongitude = JSON.parse('{"name":"longitude","value":-83.237640}');
+
+
+
+var openXCStopLatitude = JSON.parse('{"name":"latitude","value":42.291595}');
+var openXCStopLongitude = JSON.parse('{"name":"longitude","value":-83.237617}');
+var openXCFuelConsumption = JSON.parse('{"name":"fuel_consumed_since_restart","value":10}');
+var openXCFineOdometer = JSON.parse('{"name":"fine_odometer_since_restart","value":20}');
+
+var tookTime;
+//needs timestamp handling
+var timeDepart;
+//needs timestamp handling
+var timeArrive;
+
+//	var unparsedJSON = "1332794184.319404: {"name":"fuel_consumed_since_restart","value":0.090000}";
+//	var parsedTimestamp = split(unparsedJSON,1);
